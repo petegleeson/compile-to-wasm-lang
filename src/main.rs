286b56b2c22d0abdd1a4b67cf2b88b3245bb703c
Pete Gleeson <petergleeson.dev@gmail.com>
2020-07-29 20:59:30 +1000

    number and semi colon lexing

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
 use structopt::StructOpt;
 
 mod lexer {
-    #[derive(Debug, PartialEq)]
-    struct Location {
-        line: i32,
-        col: i32,
-    }
+    type Location = ((i32, i32), (i32, i32));
 
     #[derive(Debug)]
     struct LexFailure {
@@ -20,7 +16,65 @@ mod lexer {
     }
 
     fn lex(content: &str) -> Result<Vec<Token>, LexFailure> {
-        Ok(Vec::new())
+        let mut tokens = Vec::new();
+        let mut pending: Option<Token> = None;
+        let mut line = 1;
+        let mut col = 1;
+        for c in content.chars() {
+            // depending on pending and token
+            // set pending and tokens
+            match c {
+                '0'..='9' => match pending {
+                    Some(Token::Number {
+                        loc: (start, _),
+                        value,
+                    }) => {
+                        pending = Some(Token::Number {
+                            value: format!("{}{}", value, c),
+                            loc: (start, (line, col)),
+                        })
+                    }
+                    Some(token) => {
+                        pending = Some(Token::Number {
+                            value: c.to_string(),
+                            loc: ((line, col), (line, col)),
+                        });
+                        tokens.push(token)
+                    }
+                    None => {
+                        pending = Some(Token::Number {
+                            value: c.to_string(),
+                            loc: ((line, col), (line, col)),
+                        })
+                    }
+                },
+                ';' => match pending {
+                    Some(token) => {
+                        tokens.push(token);
+                        pending = None;
+                        tokens.push(Token::SemiColon {
+                            loc: ((line, col), (line, col)),
+                        })
+                    }
+                    None => tokens.push(Token::SemiColon {
+                        loc: ((line, col), (line, col)),
+                    }),
+                },
+                _ => panic!("unexpected token"),
+            }
+            // adjust line and col
+            match c {
+                // @TODO - fix for windows line endings
+                '\n' => {
+                    line = line + 1;
+                    col = 1;
+                }
+                _ => {
+                    col = col + 1;
+                }
+            }
+        }
+        Ok(tokens)
     }
 
     #[test]
@@ -30,11 +84,30 @@ mod lexer {
                 tokens,
                 [
                     Token::Number {
-                        loc: Location { line: 1, col: 1 },
+                        loc: ((1, 1), (1, 1)),
                         value: String::from("1")
                     },
                     Token::SemiColon {
-                        loc: Location { line: 1, col: 2 }
+                        loc: ((1, 2), (1, 2))
+                    }
+                ]
+            ),
+            Err(failure) => panic!(failure.message),
+        }
+    }
+
+    #[test]
+    fn it_lexs_many_numbers() {
+        match lex("420;") {
+            Ok(tokens) => assert_eq!(
+                tokens,
+                [
+                    Token::Number {
+                        loc: ((1, 1), (1, 3)),
+                        value: String::from("420")
+                    },
+                    Token::SemiColon {
+                        loc: ((1, 4), (1, 4))
                     }
                 ]
             ),
